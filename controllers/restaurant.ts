72271854d49bd4a60d4de091dc5de7fb5029f861
jeonghyeonkwon <givejeong2468@gmail.com>
2022-05-27 21:30:45 +0900

    음식 관련 트잭젝션 처리

--- controllers/restaurant.ts
+++ controllers/restaurant.ts
@@ -5,33 +5,37 @@ import Food from "../models/food";
 import Restaurant from '../models/restaurant';
 import Owner from "../models/owner";
 import {producer} from "../config/kafka";
+import {sequelize} from "../models";
+
 // /:accountRandomId
 export const createRestaurant = async (req:Request,res:Response,next:NextFunction)=>{
-
+    const transaction = await sequelize.transaction();
     try{
         const accountRandomId = req.params.accountRandomId;
 
-        // const isExistRestaurant = await Restaurant.findOne({where: {restaurantOwnerId: accountRandomId}});
-        // if(isExistRestaurant){
-        //     throw new Error('이미 음식점에 대한 정보가 있습니다');
-        // }
+        const isExistRestaurant = await Restaurant.findOne({where: {restaurantOwnerId: accountRandomId}});
+        if(isExistRestaurant){
+            throw new Error('이미 음식점에 대한 정보가 있습니다');
+        }
 
         const {restaurantName} = req.body;
         const createRestaurant = await Restaurant.create({
             restaurantName:restaurantName,
 
             restaurantId:v4()
-        });
+        },{transaction});
 
         const createOwner = await Owner.create({
             ownerId:accountRandomId
-        })
+        },{transaction})
 
-        await createRestaurant.setOwner(createOwner);
+        await createRestaurant.setOwner(createOwner,{transaction});
         await producer.send({
             topic:'restaurant-create-restaurant-event',
             messages:[{value:JSON.stringify(createRestaurant,null,2)}]
         })
+
+        await transaction.commit();
         res.status(201).send({
             statusCode:201,
             message:{
@@ -39,16 +43,17 @@ export const createRestaurant = async (req:Request,res:Response,next:NextFunctio
             }
         })
     }catch (err){
+        await transaction.rollback();
         next(err);
     }
 
 }
 // /:accountRandomId/restaurant/:restaurantRandomId/food
 export const addFood = async (req:Request,res:Response,next:NextFunction)=>{
+    const transaction = await sequelize.transaction();
     try{
         const {accountRandomId, restaurantRandomId } = req.params;
         const restaurant = await Restaurant.findOne({
-
             where:
                 {
                     restaurantId:restaurantRandomId
@@ -57,7 +62,6 @@ export const addFood = async (req:Request,res:Response,next:NextFunction)=>{
                 model:Owner,
                 required:true
             }});
-
         if(!restaurant || restaurant!.Owner.ownerId !== accountRandomId){
             throw new Error("잘못된 경로로 접근 하였습니다.");
         }
@@ -65,30 +69,40 @@ export const addFood = async (req:Request,res:Response,next:NextFunction)=>{
         // console.log(restaurant!.Owner)
 
 
-        // if(!restaurant){
-        //     throw new Error('음식점에 대한 정보가 없습니다.');
-        // }
+        
         const {foodName,foodPrice} = req.body;
         const createFood = await Food.create({
             foodName:foodName,
             foodPrice:foodPrice,
             // restaurantId:restaurant.restaurantId,
             foodId:v4()
-        });
-        await restaurant!.addFood(createFood);
-        console.log(JSON.stringify(createFood,null,2));
+        },{transaction});
+        await restaurant!.addFood(createFood,{transaction});
+
+
+        const foodMessage = {
+            id:createFood.id,
+            foodName:createFood.foodName,
+            foodPrice:createFood.foodPrice,
+            foodId:createFood.foodId,
+            updatedAt:createFood.createdAt,
+            createAt:createFood.createdAt,
+            restaurantId:restaurantRandomId,
+        }
+        console.log(JSON.stringify(foodMessage,null,2));
         await producer.send({
             topic:'restaurant-create-food-event',
-            messages:[{value:JSON.stringify(createFood,null,2)}]
+            messages:[{value:JSON.stringify(foodMessage,null,2)}]
         })
-
+        await transaction.commit();
         res.status(201).send({
             statusCode:201,
-            message : {
+            msg : {
                 foodId : createFood.foodId
             }
         })
     }catch (err){
+        await transaction.rollback();
         next(err);
     }
 }
